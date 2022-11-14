22af490bae4678f6a7abac38bfa4d2fd30a6a304
eggs-benny <benedict.smith87@gmail.com>
2022-11-14 13:47:33 +0000

    failing Account test for withdrawal

--- spec/account.test.js
+++ spec/account.test.js
@@ -15,7 +15,21 @@ describe('Account', () => {
       account.updateBalanceWithTransaction(fakeDeposit)
       expect(account.showBalance()).toEqual(1000)
     })
-  }) 
+  })
+
+  describe('#updateBalanceWithTransaction', () => {
+    it('returns a 1000 balance after 1000 deposit made', () =>{
+      const account = new Account()
+      const fakeDeposit = {deposit: 1000, withdrawal: 0}
+      const fakeDeposit2 = {deposit: 2000, withdrawal: 0}
+      const fakeWithdrawal = {deposit: 0, withdrawal: 500}
+      account.updateBalanceWithTransaction(fakeDeposit)
+      account.updateBalanceWithTransaction(fakeDeposit2)
+      account.updateBalanceWithTransaction(fakeWithdrawal)
+      expect(account.showBalance()).toEqual(2500)
+    })
+  })
+})
 
   // describe('#deposit', () => {
   //   it('returns a 1000 balance after a 1000 deposit', () => {
@@ -34,4 +48,3 @@ describe('Account', () => {
   //     expect(account.showBalance()).toEqual(2500);
   //   });
   // });
-});
