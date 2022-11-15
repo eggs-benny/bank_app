07212d863e2c8be0b7eb1bdf6556675284e4d582
eggs-benny <benedict.smith87@gmail.com>
2022-11-15 14:42:54 +0000

    failing tests for 0 or negative input errors

--- spec/accountManager.test.js
+++ spec/accountManager.test.js
@@ -21,6 +21,38 @@ describe('AccountManager', () => {
       expect(accountManager.printStatement()).toMatch('10/01/2023 || 1000.00 || || 1000.00')
     })
 
+    it('returns error if deposit is -ve', () =>{
+      const t = () => {
+        accountManager.makeDeposit(-1000)
+      };
+        expect(t).toThrow(Error)
+        expect(t).toThrow(`Deposit can't be <= 0. If you meant to withdraw, use makeWithdrawal() function.`)
+    })
+
+    it('returns error if deposit is 0', () =>{
+      const t = () => {
+        accountManager.makeDeposit(0)
+      };
+        expect(t).toThrow(Error)
+        expect(t).toThrow(`Deposit can't be <= 0. If you meant to withdraw, use makeWithdrawal() function.`)
+    })
+
+    it('returns error if withdrawal is -ve', () =>{
+      const t = () => {
+        accountManager.makeWithdrawal(-1000)
+      };
+        expect(t).toThrow(Error)
+        expect(t).toThrow(`Withdrawal can't be <= 0. If you meant to deposit, use makeDeposit() function.`)
+    })
+
+    it('returns error if withdrawal is 0', () =>{
+      const t = () => {
+        accountManager.makeWithdrawal(0)
+      };
+        expect(t).toThrow(Error)
+        expect(t).toThrow(`Withdrawal can't be <= 0. If you meant to deposit, use makeDeposit() function.`)
+    })
+
     it('returns a statement displaying two deposits', () =>{
       accountManager.makeDeposit(1000)
       accountManager.makeDeposit(2000)
